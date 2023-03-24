1c217bdfdcab388deda0c9922427f4f547ff8fc0
yash17-12-98 <sutharharsh311@gmail.com>
2023-03-24 07:53:52 +0530

    added file for filter

---
+++ src/components/Allposted.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">All posted Data</div>
+      <div v-for="Lists in List" :key="Lists.id" class="card-body">
+        <h5 class="card-title">{{ Lists.title }}</h5>
+        <p class="card-text">{{ Lists.body }}</p>
+        <a href="#" @click="Checkcomments(Lists.id)" class="btn btn-primary"
+          >comments</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Addusers-item",
+  data() {
+    return {
+      List: "",
+    };
+  },
+
+  mounted() {
+    //   let id = this.$route.params.id
+    axios
+      .get(`http://jsonplaceholder.typicode.com/posts`)
+      .then((response) => (this.List = response.data));
+  },
+  methods: {
+    Checkcomments(e) {
+      console.log(e);
+      this.$router.push({ name: "CommentsList", params: { id: e } });
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/CommentsList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="card">
+        <router-link :to="{ path: '/home/' + this.$route.params.id }">All-posts</router-link>
+
+      <div class="card-header">CommentsList</div>
+      <div v-for="Lists in List" :key="Lists.id" class="card-body">
+        <h5 class="card-title">{{ Lists.email }}</h5>
+        <p class="card-text">{{ Lists.body }}</p>
+        <!-- <a href="#" @click="Checkcomments(Lists.id)" class="btn btn-primary">comments</a> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      List: "",
+    };
+  },
+  mounted() {
+    let id = this.$route.params.id;
+    axios
+      .get(`http://jsonplaceholder.typicode.com/comments?postId=${id}`)
+      .then((response) => {
+        this.List = response.data;
+      });
+  },
+};
+</script>
+
+<style></style>
